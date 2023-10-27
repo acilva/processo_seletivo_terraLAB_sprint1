ed24c616baa948689dd489c81449a8610b226405
ana <acilva014@gmail.com>
2023-10-27 09:13:30 -0300

    feat:added subtraction

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,40 @@
+//questão 3
 /*
 const args = process.argv.slice(2);
 console.log(parseInt(args[0]) + parseInt(args[1]));*/
 
+//questão 5
+/*
 const soma = () => {
     console.log(parseInt(args[0]) + parseInt(args[1]));
 };
 
 const args = process.argv.slice(2);
 
-soma();
+soma();*/
+
+//questão 6
+const soma = () => {
+    console.log(parseInt(args[1]) + parseInt(args[2]));
+};
+
+const sub = () => {
+    console.log(parseInt(args[1]) - parseInt(args[2]));  
+}
+
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+    case 'soma':
+        soma();
+    break;
+
+    case 'sub':
+        sub();
+    break;
+
+    default:
+        console.log('does not support', arg[0]);
+}
+
 
