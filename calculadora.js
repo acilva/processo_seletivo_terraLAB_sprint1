d3040bd1656c044ece81ac9531d89d3ea9a1093a
ana <acilva014@gmail.com>
2023-10-26 21:19:30 -0300

    fix:modified code script.js

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,12 @@
+/*
+const args = process.argv.slice(2);
+console.log(parseInt(args[0]) + parseInt(args[1]));*/
+
+const soma = () => {
+    console.log(parseInt(args[0]) + parseInt(args[1]));
+};
 
 const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]));
+
+soma();
 
